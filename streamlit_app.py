9388b74aea13b4a1946264928f76a4187cf0bccd
Not a fish <115078895+t67if43r43@users.noreply.github.com>
2022-10-06 08:44:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fstreamlit.text(fruityvice_response)ruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
