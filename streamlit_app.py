37cbf41ac5ac774513e275261e94b03610df134f
Not a fish <115078895+t67if43r43@users.noreply.github.com>
2022-10-06 08:43:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fstreamlit.text(fruityvice_response)ruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
